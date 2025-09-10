16ad959833114a935f05532a9f8c7edf85614f4d
matt-vanwyk <matt.vanwyk2@gmail.com>
2025-09-10 11:08:27 +0200

    Created UploadMission.srv in shared_interfaces package. This service is used to upload a mission to the drone from the base station once the drone has confirmed that it is ready to receive a mission via the PrepareForMission.srv

--- ros2_ws/src/base_station_package/base_station_package/base_station_state_machine.py
+++ ros2_ws/src/base_station_package/base_station_package/base_station_state_machine.py
@@ -4,7 +4,7 @@ import time
 from rclpy.node import Node
 from statemachine import State, StateMachine
 from shared_interfaces.msg import DroneState
-from shared_interfaces.srv import PrepareForMission
+from shared_interfaces.srv import PrepareForMission, UploadMission
 
 class BaseStationStateMachine(StateMachine):
     """State machine definitions"""
@@ -53,16 +53,27 @@ class BaseStationStateMachineNode(Node):
             PrepareForMission,
             'drone/prepare_for_mission'
         )
+        self.upload_mission_client = self.create_client(
+            UploadMission,
+            'drone/upload_mission'
+        )
 
         # Timer for testing - triggers mission after 5 seconds
         self.create_timer(5.0, self.trigger_test_mission)
-        self.mission_triggered = False  # Flag to only trigger once
+        self.mission_triggered = False
 
         self.drone_state = DroneState()
 
-    def drone_state_callback(self, msg):
-        self.drone_state = msg
-        #self.get_logger().info(f'Received Drone State: {msg.current_state}')
+        ############################
+        # TODO - REMOVE once websocket API is integrated for mission requests
+        ############################
+        # Mission Variables (hardcoded for testing)
+        self.current_mission_id = "test_mission_001"
+        self.current_waypoints = []
+
+###################################
+# METHODS FOR CLIENT SERVICE CALLS TO DRONE STATE MACHINE
+###################################
 
     def prepare_drone_for_mission(self, mission_id, waypoints):
         """Calls drone service to prepare for mission"""
@@ -82,6 +93,7 @@ class BaseStationStateMachineNode(Node):
                     self.get_logger().info("Drone preparation successful - uploading mission")
 
                     self.state_machine.uploading_mission()
+                    self.upload_mission_to_drone(self.current_mission_id, self.current_waypoints)
                 else:
                     self.get_logger().error(f'Drone not ready: {response.error_message}')
                     self.get_logger().error("Drone preparation failed - aborting mission")
@@ -92,15 +104,48 @@ class BaseStationStateMachineNode(Node):
         future = self.prepare_mission_client.call_async(request)
         future.add_done_callback(drone_preparation_callback)
 
+    def upload_mission_to_drone(self, mission_id, waypoints):
+        """Upload mission to drone"""
+        if not self.upload_mission_client.wait_for_service(timeout_sec=5.0):
+            self.get_logger().error('Drone upload mission service not available')
+            return
+        
+        request = UploadMission.Request()
+        request.mission_id = mission_id
+        request.waypoints = waypoints
+        
+        def mission_upload_callback(future):
+            try:
+                response = future.result()
+                if response.success:
+                    self.get_logger().info(f'Mission uploaded: {response.num_waypoints_received} waypoints')
+                    self.get_logger().info("Mission upload successful - ready for Step 3")
+                    # TODO: Transition to next state (uncentering arms)
+                else:
+                    self.get_logger().error(f'Mission upload failed: {response.error_message}')
+            except Exception as e:
+                self.get_logger().error(f'Mission upload service call failed: {str(e)}')
+        
+        future = self.upload_mission_client.call_async(request)
+        future.add_done_callback(mission_upload_callback)
+
+###################################
+# METHODS FOR CLIENT SERVICE CALLS TO DRONE STATE MACHINE
+###################################
+
 #############################
-#SIMULATION
+#MISSION SIMULATION - TODO self.state_machine.prepare_drone() transiton needs to altered to happen 
+# when the base station receives a mission request from cloud via websockets.
+# When mission request is received, base station should go from 'idle' state to 'mission received' state.
+# Then in 'mission received' state, base station needs to check its own state (doors closed, arms centred, charging etc)
+# and if all conditions are met, it can then call the drone service to prepare for mission and switch to 'preparing_drone' state.
 #############################
         
     def simulate_mission_received(self):
         """Simulate receiving a mission from API - for testing"""
         from shared_interfaces.msg import Waypoint
         
-        # Create test waypoints
+        # Create test waypointsit should call the prepare_drone_for_mission() method to prepare the drone.
         waypoints = []
         waypoint1 = Waypoint()
         waypoint1.latitude = -33.9249
@@ -108,12 +153,14 @@ class BaseStationStateMachineNode(Node):
         waypoint1.altitude = 50.0
         waypoint1.speed = 5.0
         waypoints.append(waypoint1)
+
+        self.current_waypoints = waypoints
         
         # Transition to preparing_drone state
         self.state_machine.prepare_drone()
         
         # Call drone service (now async with callbacks)
-        self.prepare_drone_for_mission("test_mission_001", waypoints)
+        self.prepare_drone_for_mission(self.current_mission_id, self.current_waypoints)
 
     def trigger_test_mission(self):
         """Timer callback to trigger test mission"""
@@ -127,6 +174,18 @@ class BaseStationStateMachineNode(Node):
 
 #############################
 #SIMULATION
+#############################
+
+#############################
+#NOT BEING USED
+#############################
+
+    def drone_state_callback(self, msg):
+            self.drone_state = msg
+            #self.get_logger().info(f'Received Drone State: {msg.current_state}')
+
+#############################
+#NOT BEING USED
 #############################
 
 def main():
